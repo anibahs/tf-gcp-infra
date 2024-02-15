b2fda18dd0d5a0ef554f46fe4ea32012a716358b
Shabina Singh <anibahs@gmail.com>
2024-02-15 10:40:50 -0500

    added variables

--- main.tf
+++ main.tf
@@ -6,30 +6,30 @@ provider "google" {
 }
 
 resource "google_compute_network" "custom-vpc" {
-  name                            = "custom-vpc"
+  name                            = var.custom_vpc
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "db-subnet" {
-  name                     = "db-subnet"
-  ip_cidr_range            = "10.1.2.0/24"
+  name                     = var.db_subnet
+  ip_cidr_range            = var.db_cidr
   region                   = var.gcp_region
   network                  = google_compute_network.custom-vpc.id
   private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "webapp-subnet" {
-  name                     = "webapp-subnet"
-  ip_cidr_range            = "10.1.3.0/24"
-  region                   = var.gcp_region
-  network                  = google_compute_network.custom-vpc.id
+  name          = var.webapp_subnet
+  ip_cidr_range = var.webapp_cidr
+  region        = var.gcp_region
+  network       = google_compute_network.custom-vpc.id
 }
 
 resource "google_compute_route" "network-route" {
-  name              = "network-route"
-  dest_range        = "0.0.0.0/0"
-  network           = google_compute_network.custom-vpc.name
-  next_hop_gateway  = "default-internet-gateway"
-}
+  name             = var.network_route
+  dest_range       = var.public_gateway
+  network          = google_compute_network.custom-vpc.name
+  next_hop_gateway = "default-internet-gateway"
+} 
